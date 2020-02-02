74872c3d5a74e29d62325abc8849942057876b0d
stern118 <stern118@users.noreply.github.com>
2020-02-02 21:02:06 +0900

    DBを使用するように変更

--- main.go
+++ main.go
@@ -1,43 +1,53 @@
 package main
 
 import (
+	"context"
+	"database/sql"
+	"fmt"
+	"gin-todo/models"
 	"net/http"
 
 	"github.com/gin-gonic/gin"
+	_ "github.com/go-sql-driver/mysql"
+	"github.com/volatiletech/sqlboiler/boil"
+	// . "github.com/volatiletech/sqlboiler/queries/qm"
 )
 
 //ルーティングの設定
-func setupRouter(tasksPtr *[]string) *gin.Engine {
+func setupRouter() *gin.Engine {
 	r := gin.Default()
 	// HTMLファイル読み込み
 	r.LoadHTMLGlob("app/templates/*")
 
 	// 初期表示
 	r.GET("/", func(c *gin.Context) {
+		ctx := context.Background()
+		db, err := sql.Open("mysql", "root:root@tcp(127.0.0.1:3300)/todos?parseTime=true")
+		if err != nil {
+			panic(err.Error())
+		}
+		defer db.Close()
+
+		boil.DebugMode = true
+		boil.SetDB(db)
+		tasks, err := models.Tasks().All(ctx, db)
+		if err != nil {
+			fmt.Printf("Get tasks error: %v", err)
+		}
 		c.HTML(http.StatusOK, "index.html", gin.H{
-			"tasks": *tasksPtr,
+			"tasks": tasks,
 		})
 	})
 
 	// タスク作成
 	r.POST("/create", func(c *gin.Context) {
-		// tasks sliceにPost Dataを追加
-		tasks := append(*tasksPtr, c.PostForm("task"))
-		c.HTML(http.StatusOK, "index.html", gin.H{
-			"tasks": tasks,
-		})
+		c.HTML(http.StatusOK, "index.html", gin.H{})
 	})
 
 	return r
 }
 
 func main() {
-	// taskリスト
-	var tasks []string
-	tasks = append(tasks, "Goの勉強をする")
-	tasks = append(tasks, "バスのチケットを取る")
-	tasks = append(tasks, "履歴書を書く")
-
-	r := setupRouter(&tasks)
+	r := setupRouter()
 	r.Run(":8080")
 }
